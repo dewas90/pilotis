5f46eacddf3aa81211f14f91d84fd4dfcbd7a883
Reginald de Wasseige <reginalddewasseige@gmail.com>
2017-11-13 15:14:51 +0100

    controllers done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :profiles, except: [:destroy]
+
+  resources :invoices, except: [:destroy] do
+    member do
+      post 'paid', to: 'invoices#paid'
+      post 'unpaid', to: 'invoices#unpaid'
+    end
+  end
+
 end
