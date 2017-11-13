131899eb5c39c36b1c7caf6a020fef936929fff8
Carmen Longo <carmenlongo@hotmail.com>
2017-11-13 16:49:56 +0100

    controller profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,41 @@
 class ProfilesController < ApplicationController
+  before_action :set_profile, only: [:show, :edit, :update]
+
   def index
+    @profiles = Profile.all
   end
 
   def show
   end
 
   def new
+    @profile = Profile.new
   end
 
   def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to profile_path(@profile)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @profile.update(profile_params)
+    redirect_to profile_path(@profile)
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :totem, :birth_date, :address, :zip_code, :city, :country, :phone_number, :photo, :gender, :comment)
+  end
+
+  def set_profile
+    @profile = Profile.find(params[:id])
   end
 end
