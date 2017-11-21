19ea62b848b79d64a7bddef01690e3103636c6b2
Diogo Heinen <eltigredc@gmail.com>
2017-11-21 21:17:23 +0100

    mark as paid works

---
+++ app/assets/javascripts/t6_markaspaid.js
@@ -0,0 +1,29 @@
+const bton = document.getElementById("paidbtn");
+let ids = []
+function select(){
+  const line = document.querySelectorAll(".selected");
+  line.forEach(function(item){
+    ids.push(item.dataset.id)
+     ids.forEach(function(id){
+      $.ajax({
+      type:"POST",
+      url:'/invoices/markaspaid/',
+      data: {invoice_id: id}
+    });
+    });
+    // $.ajax({
+    //   url:'/invoices/markaspaid',
+    //   data: "invoice.id=" + item
+    // });
+
+  });
+  console.log(ids);
+};
+
+// function get(){
+//   ids.push(document.querySelectorAll(".selected").dataset.id)
+// };
+
+bton.addEventListener("click", select, false);
+
+
