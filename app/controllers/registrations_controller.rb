793bb30fad3faa052d21c020702d2cf110510905
yasser amir baron dao <yass@yassers-MacBook-Pro.local>
2017-11-23 14:14:12 +0100

    fixing the exclution of sidebar in the signup and login page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
+  def new
+  	super do
+  	@disable_sidebar = true
+  	end
+  end	
+
   protected
+  
 
   def after_sign_up_path_for(resource)
     '/profiles/new' # Or :prefix_to_your_route
   end
+
+
 end
